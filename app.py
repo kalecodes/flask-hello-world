7ef6053eebe7cab064343b4cc561ac52b915a1b7
Kalen Wiley <kawi8065@colorado.edu>
2025-11-15 01:24:06 +0000

    try error fix

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World from Kalen Wiley in 3308!'
 
-@app.route('db_test')
-def db_test():
+@app.route('/db_test')
+def testing():
     conn = psycopg2.connect("postgresql://kalecodes_flask_hello_world_user:zJUIuYJGjYiF3UjwZKTtA5ZcKW46WKmn@dpg-d4bt760gjchc73d0ceag-a/kalecodes_flask_hello_world")
+    conn.close()
+    return "Database Connection Successful"
