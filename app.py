bf2802e74def3a541c84c2c784b21535cf8d0d45
Kalen Wiley <kawi8065@colorado.edu>
2025-11-15 01:46:19 +0000

    add additional db routes

--- app.py
+++ app.py
@@ -28,3 +28,53 @@ def creating():
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Created"
+
+@app.route('db_insert')
+def inserting():
+    conn = psycopg2.connect("postgresql://kalecodes_flask_hello_world_user:zJUIuYJGjYiF3UjwZKTtA5ZcKW46WKmn@dpg-d4bt760gjchc73d0ceag-a/kalecodes_flask_hello_world")
+    cur = conn.cursor()
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Populated"
+
+@app.route('db_select')
+def selecting():
+    conn = psycopg2.connect("postgresql://kalecodes_flask_hello_world_user:zJUIuYJGjYiF3UjwZKTtA5ZcKW46WKmn@dpg-d4bt760gjchc73d0ceag-a/kalecodes_flask_hello_world")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+    ''')
+    records = cur.fetchall()
+    conn.close()
+    response_string=""
+    response_string+="<table>"
+    for player in records:
+        response_string+="<tr>"
+        for info in player:
+            response_string+="<td>{}</td>".format(info)
+        response_string+="</tr>"
+    response_string+="</table>"
+    return response_string
+
+@app.route('/db_drop')
+def dropping():
+    conn = psycopg2.connect("postgresql://kalecodes_flask_hello_world_user:zJUIuYJGjYiF3UjwZKTtA5ZcKW46WKmn@dpg-d4bt760gjchc73d0ceag-a/kalecodes_flask_hello_world")
+    cur = conn.cursor()
+    cur.execute('''
+        DROP TABLE Basketball;
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Dropped"
+
+
+
+
